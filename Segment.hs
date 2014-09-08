3eaab9854d49371d26797cb9f568047bf5e3dca1
Sean <burton.seanr@gmail.com>
2014-09-08 01:21:10 +0100

    Cleaned up the IO and file processing. Note that I have pulled out some of the functionality so I can integrate it more cleanly later.

--- Segment.hs
+++ Segment.hs
@@ -1,73 +1,46 @@
-{-# LANGUAGE OverloadedStrings, TupleSections #-}
+{-# LANGUAGE OverloadedStrings, TupleSections, BangPatterns #-}
 
-import qualified Data.Text as T
-import qualified Data.Map as M
+import Data.Char (isSpace)
+import Data.Text.ICU.Char (blockCode, BlockCode(CJKUnifiedIdeographs))
 import qualified Data.Text.Lazy as TL
 import qualified Data.Text.Lazy.IO as TL.IO
-import Data.List
-import Data.Function (on)
-import Data.Text.ICU.Char
-import CJK.Data.Pinyin
-import CJK.Data.Unihan.Readings
+import System.Environment (getArgs)
 import System.FilePath ((</>))
-import System.Process 
-import System.Directory
+import System.FilePath.Posix (takeBaseName)
+import System.Process (system)
 
-data FlashCard = FlashCard {word :: T.Text,
-                            segmented :: [T.Text],
-                            unsegmented :: T.Text,
-                            pronunciation :: T.Text,
-                            definition :: T.Text,
-                            cloze :: T.Text} deriving (Show)
-                            
 segmenterPath :: String 
 segmenterPath = "stanford-segmenter-2014-08-27"
 
-segment  :: FilePath -> IO TL.Text
-segment filePath = do system ("." </> segmenterPath </> "segment.sh ctb " ++ filePath ++ " utf8 0 > /dev/null | tee temp.seg") 
-                      segmented <- TL.IO.readFile ("Temp" </> "seg.temp")
-                      return segmented
-                      
-writeSentences :: String -> [TL.Text] -> IO()
-writeSentences path = TL.IO.writeFile (path ++ "-temp" </> "sentences.temp") $ unlines sentences
+temp :: String -> String -> String
+temp category filePath = "Temp" </> takeBaseName filePath ++ "-" ++ category ++ ".tmp"
 
-sentencesFromFile :: String -> IO()
-sentencesFromFile = 
-                    
-sentences :: TL.Text -> [TL.Text]
-sentences = TL.splitWhen (\c -> c == '.' || c == "。")
-                      
+segment_cmd :: String -> String -> String
+segment_cmd from to = "." </> segmenterPath </> "segment.sh ctb " ++ from ++ " utf8 0 | tee " ++ to
+
+segment  :: FilePath -> FilePath -> IO TL.Text
+segment from to = do system $ segment_cmd from to
+                     segmented <- TL.IO.readFile to
+                     return segmented
+                                      
 isHanzi :: Char -> Bool
 isHanzi = (==CJKUnifiedIdeographs) . blockCode
-
-chineseWords :: TL.Text -> [TL.Text]
-chineseWords = filter (TL.all $ \c -> c == ' ' || isHanzi c) . TL.words
-             
-occurences :: [(TL.Text, [TL.Text])] -> M.Map TL.Text [TL.Text]
-occurences = foldl (\m (unsegmented, segmented) -> foldl (occursIn unsegmented) m segmented) M.empty 
-    where occursIn sentence m word = M.insertWith (const (sentence:)) word [] m
-  
-reading :: TL.Text -> TL.Text
-reading = TL.fromStrict . T.concatMap (toAccented . head . mandarinBestEffort) . TL.toStrict
-          
-cloze :: TL.Text -> TL.Text -> TL.Text -> TL.Text
-cloze hint w s = TL.replace w clozeText s
-    where hint' = if TL.null hint 
-                      then hint 
-                      else "::" `TL.append` hint 
-          clozeText = TL.concat ["{{c1::", w, hint', "}}"]
-
-flashCards :: M.Map TL.Text [TL.Text] -> [TL.Text]
-flashCards =  map snd . concatMap (\(a, b) -> map (a,) $ take 10 b) . sortBy (compare `on` (negate . length . snd)) . M.toList 
-
-muddle :: Int
-muddle = undefined
-         
-main :: IO ()
-main = segment "blah" >>= (TL.IO.writeFile "out.txt" . TL.unlines . flashCards . occurences)
-          
-          
-                            
                       
-  
-                      
+sentences :: TL.Text -> [TL.Text]
+sentences = filter (TL.any isHanzi) . map (TL.strip) . TL.split (\c -> c == '.' || c == '。') . TL.filter (not . isSpace)
+
+preprocess :: FilePath -> FilePath -> IO [TL.Text]
+preprocess from to = do body <- TL.IO.readFile from
+                        let sentences' = sentences body
+                        TL.IO.writeFile to $ TL.unlines sentences'
+                        return sentences'
+                                            
+main :: IO ()                      
+main = do [filePath] <- getArgs
+          let sentenceFile = temp "sentences" filePath
+          let segmentFile  = temp "segmented-sentences" filePath
+          sentences' <- preprocess filePath sentenceFile
+          segmented  <- segment sentenceFile segmentFile
+          return ()
+          
+
